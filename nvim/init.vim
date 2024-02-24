aaf642c54fba189a027b43ca3489fde728519bca
zarool <karol.zachura01@gmail.com>
2024-02-25 00:09:00 +0100

    Fixed links issue

---
+++ nvim/init.vim
@@ -0,0 +1,16 @@
+:set number
+:set relativenumber
+
+:set mouse=a
+
+:set autoindent
+:set tabstop=4
+:set shiftwidth=4
+:set smarttab
+:set softtabstop=4
+
+call plug#begin()
+
+Plug 'https://github.com/vim-airline/vim-airline'
+
+call plug#end()
---
+++ polybar/launch.sh
@@ -0,0 +1,22 @@
+﻿#!/usr/bin/env bash
+
+# Terminate already running bar instances
+killall -q polybar
+setxkbmap pl
+# If all your bars have ipc enabled, you can also use 
+# polybar-msg cmd quit
+
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=$m polybar --reload example &
+  done
+else
+  polybar --reload example &
+fi
+
+
+# Launch bar1 and bar2
+#echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
+#polybar example 2>&1 | tee -a /tmp/polybar1.log & disown
+
+echo "Bars launched..."
